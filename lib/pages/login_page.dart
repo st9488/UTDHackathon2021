41ea72345fb20f0d4a240557c35365d829defcd4
Matthew Chimitt <mmchimitt@gmail.com>
2021-11-13 17:38:33 -0600

    Fixing main

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:utd_hackathon2021/UI/background.dart';
-import 'package:utd_hackathon2021/util/claim.dart';
-import 'package:utd_hackathon2021/util/globals.dart';
-import 'package:utd_hackathon2021/util/services/auth.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -15,200 +13,94 @@ class LoginPage extends StatelessWidget {
       alignment: Alignment.center,
       children: [
         const Background(),
+        Padding(
+        padding: const EdgeInsets.fromLTRB(40, 80, 40, 0),
+        child:
         Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(width: 120, height: 120, child:Image(image: AssetImage('assets/graphics/logo.png'))),
-            const Padding(padding: EdgeInsets.symmetric(vertical: 5)),
-            Container(
-              width: 325,
-              height: 180,
-              decoration: BoxDecoration(
-                  color: const Color.fromRGBO(93, 93, 93, 0.18),
-                  border: Border.all(
-                    color: const Color.fromRGBO(93, 93, 93, 0.18),
-                  ),
-                  borderRadius: const BorderRadius.all(Radius.circular(20))
-              ),
-              //color: Colors.grey,
-              child: Column(
-                children: [
-                  Container(
-                    height: 10,
-                  ),
-                  const Text(
-                    'ClaimSafe',
-                    style: TextStyle(decoration: TextDecoration.none, fontFamily: 'BabasNeue'),
-                  ),
-                  Container(
-                    height: 20,
-                  ),
+          const Text(
+            'ClaimSafe',
+            style: TextStyle(decoration: TextDecoration.none, fontFamily: 'BabasNeue'),
+          ),
 
-                  SizedBox(
-                    width: 250,
-                    child: CupertinoButton(
-                      color: Theme.of(context).colorScheme.background,
-                      child: const Text(
-                        "I got into an accident!",
-                        style: TextStyle(color: Colors.white,),
-                        textAlign: TextAlign.center,
-                      ),
-
-                      onPressed: () {
-                        currentClaim = Claim();
-                        Navigator.of(context).pushReplacementNamed('/Panic');
-                      },
-                    ),
-                  ),
-                ],
-              ),
+            Padding(padding: const EdgeInsets.fromLTRB(30, 10, 30, 0),
+                child:
+                  CupertinoButton(
+                    color: Theme.of(context).colorScheme.background,
+                    child: const Text("File a Claim Now"),
+                    onPressed: () {
+                    print("Debug: File Claim Button has been clicked.");
+                    },
+                  )
             ),
-            const Padding(padding: EdgeInsets.symmetric(vertical: 20)),
-
-            Container(
-                width: 325,
-                height: 390,
-                decoration: BoxDecoration(
-                    color: const Color.fromRGBO(93, 93, 93, 0.18),
-                    border: Border.all(
-                      color: const Color.fromRGBO(93, 93, 93, 0.18),
-                    ),
-                    borderRadius: const BorderRadius.all(Radius.circular(20))),
-                child: const LoginInfo()
-
-            ),
-          ],
+            const Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+              child:
+                LoginInfo(),
+              ),
+            ],
+          ),
         ),
       ],
 
-
     );
   }
 }
 
 
-class LoginInfo extends StatefulWidget{
+class LoginInfo extends StatelessWidget{
   const LoginInfo({Key? key}) : super(key: key);
 
   @override
-  State<LoginInfo> createState() => _LoginInfoState();
-}
-
-class _LoginInfoState extends State<LoginInfo> {
-
-  final AuthService _auth = AuthService();
-  final _formKey = GlobalKey<FormState>();
-
-  final controllerUserSignIn = TextEditingController();
-  final controllerPassSignIn = TextEditingController();
-
-
-  @override
   Widget build (BuildContext context){
-    return Form(
-      key: _formKey,
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'Login',
-              style: TextStyle(decoration: TextDecoration.none, fontFamily: 'BabasNeue'),
-            ),
-
-            Container(
-              height: 25,
-            ),
-
-            SizedBox(
-              width: 300,
-              height: 45,
-
-              child: CupertinoTextField(
-                controller: controllerUserSignIn,
-                placeholder: 'Username',
-                placeholderStyle: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                      color: Colors.white,
-                    ),
-                    borderRadius: const BorderRadius.all(Radius.circular(10))
-
-                ),
-                style: const TextStyle(color: Colors.black),
-                textAlign: TextAlign.center,
-              ),
-            ),
-
-            Container(
-              height: 15,
-            ),
-
-            SizedBox(
-              width: 300,
-              height: 45,
-              child: CupertinoTextField(
-                controller: controllerPassSignIn,
-                placeholder: 'Password',
-                placeholderStyle: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                      color: Colors.white,
-                    ),
-                    borderRadius: const BorderRadius.all(Radius.circular(10))
-
-                ),
-                style: const TextStyle(color: Colors.black),
-                textAlign: TextAlign.center,
-              ),
-            ),
-
-            Container(
-              height: 30,
-            ),
-
-            SizedBox(
-              width: 250,
-              child: CupertinoButton(
-                  color: Theme.of(context).colorScheme.background,
-                  child: const Text("Sign In", style: TextStyle(color: Colors.white)),
-                  onPressed: () async {
-                    dynamic result = await _auth.signInWithEmailAndPassword(controllerUserSignIn.text, controllerPassSignIn.text);
-                    if(result == null){
-                      print("error: user not found");
-                    } else {
-                      currentClaim = Claim();
-                      currentUserSelf = result;
-                      Navigator.of(context).pushReplacementNamed('/Main');
-                    }
-                  }
-              ),
-            ),
-
-            Container(
-              height: 25,
-            ),
-
-
-            SizedBox(
-              width: 250,
-              child: CupertinoButton(
-                  color: Theme.of(context).colorScheme.background,
-                  child: const Text("Create Account", style: TextStyle(color: Colors.white), textAlign: TextAlign.center,),
-                  onPressed: () async {
-                    print(controllerPassSignIn.text + " - " + controllerUserSignIn.text);
-                    currentUserSelf = await _auth.registerWithEmailAndPassword(controllerUserSignIn.text, controllerPassSignIn.text);
-                    Navigator.of(context).pushReplacementNamed('/Main');
-                  }
-              ),
-            ),
-          ]
-      ),
+    return Column(
+      children: [
+        const Text(
+          'Login',
+          style: TextStyle(decoration: TextDecoration.none, fontFamily: 'BabasNeue'),
+        ),
+        const InputFields(),
+        CupertinoButton(
+          color: Theme.of(context).colorScheme.background,
+          child: const Text("Submit"),
+          onPressed: () {
+            print("Debug: Login Button has been clicked.");
+          }
+        ),
+      ]
     );
   }
+}
 
+class InputFields extends StatefulWidget{
+  const InputFields({Key? key}) : super(key: key);
 
+  @override
+  State<InputFields> createState() => _InputFieldsState();
+}
+
+class _InputFieldsState extends State<InputFields> {
+  @override
+  Widget build (BuildContext context){
+    return 
+      Padding( padding: const EdgeInsets.fromLTRB(0, 5, 0, 20) ,
+      child: Column(
+        children: const [
+          Padding(padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child:
+                CupertinoTextField(
+                placeholder: 'Username',
+                style: TextStyle(color: Colors.white),
+                )
+          ),
+          Padding(padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child:
+                CupertinoTextField(
+                  placeholder: 'Password',
+                  style: TextStyle(color: Colors.white),
+                )
+          ),
+        ],
+      )
+    );
+  }
 }
