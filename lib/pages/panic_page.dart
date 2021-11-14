a1adb09af6eabaa770adf73c09eda324aac83392
mkm-programmer <53355393+mkm-programmer@users.noreply.github.com>
2021-11-14 05:22:12 -0600

    All my updated stuff

--- lib/pages/panic_page.dart
+++ lib/pages/panic_page.dart
@@ -7,117 +7,149 @@ class PanicPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        const Background(),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text("First and foremost, make sure to call the police",
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontSize: 25,
-                    color: Colors.red
-                )
-            ),
-            Container(height: 40,),
-
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text("Inform the police of: " ,
-                    style: TextStyle(
-                      color: Colors.red,
-                      decoration: TextDecoration.none,
-                      fontSize: 20,
-                    )
-                ),
-                Container(height: 20,),
-                Text("• Date and time of the accident" ,
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary,
-                        decoration: TextDecoration.none,
-                        fontSize: 15
-                    )
-                ),
-                Text("• Personal information of the parties involved" ,
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary,
-                        decoration: TextDecoration.none,
-                        fontSize: 15
-                    )
-                ),
-                Text("• Statements from the drivers" ,
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary,
-                        decoration: TextDecoration.none,
-                        fontSize: 15
-                    )
-                ),
-                Text("• Statements from any witnesses" ,
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary,
-                        decoration: TextDecoration.none,
-                        fontSize: 15
-                    )
-                ),
-                Text("• Information about the vehicles involved in the accident" ,
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary,
-                        decoration: TextDecoration.none,
-                        fontSize: 15
-                    )
-                ),
-                Text("• Description of the road and any other environmental issues" ,
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary,
-                        decoration: TextDecoration.none,
-                        fontSize: 15
-                    )
-                ),
-                Text("• Any video or surveillance equipment that may have documented the accident" ,
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary,
-                        decoration: TextDecoration.none,
-                        fontSize: 15
-                    )
-                ),
-                Text("• Injuries" ,
-                    style: TextStyle(color: Theme.of(context).colorScheme.primary,
-                        decoration: TextDecoration.none,
-                        fontSize: 15
-                    )
-                ),
-                Container(height: 30),
-              ],
-            ),
-            Column(
+    return SizedBox(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          const Background(),
+          SizedBox(
+            width: 350,
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(
-                  width: 275,
-                  height: 50,
-                  child: CupertinoButton(
-                      color: Theme.of(context).colorScheme.primary,
-                      child: Text('Call Police', style: TextStyle(color: Theme.of(context).colorScheme.onBackground)),
-                      onPressed: () {}
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text("First and foremost, make sure to call the police",
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 20
+                        )
+                    ),
                   ),
                 ),
-                Container(height: 30),
-                SizedBox(
-                  width: 275,
-                  height: 50,
-                  child: CupertinoButton(
-                      color: Theme.of(context).colorScheme.primary,
-                      child: Text('Next', style: TextStyle(color: Theme.of(context).colorScheme.onBackground)),
-                      onPressed: () {
-                        Navigator.of(context).pushReplacementNamed('/Tips');
-                      }
+                Container(height: 20,),
+
+                Container(
+                  width: 375,
+                  height: 340,
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                    child: Column(
+                      //mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(height: 15),
+                        const Text("Inform the police of: " ,
+                            style: TextStyle(
+                              color: Colors.black,
+                              decoration: TextDecoration.none,
+                              fontSize: 20,
+                            )
+                        ),
+                        Container(height: 20),
+                        Text("• Date and time of the accident" ,
+                            style: TextStyle(color: Colors.white,
+                                decoration: TextDecoration.none,
+                                fontSize: 15
+                            )
+                        ),
+                        Text("• Personal information of the parties involved" ,
+                            style: TextStyle(color: Colors.white,
+                                decoration: TextDecoration.none,
+                                fontSize: 15
+                            )
+                        ),
+                        Text("• Statements from the drivers" ,
+                            style: TextStyle(color: Colors.white,
+                                decoration: TextDecoration.none,
+                                fontSize: 15
+                            )
+                        ),
+                        Text("• Statements from any witnesses" ,
+                            style: TextStyle(color: Colors.white,
+                                decoration: TextDecoration.none,
+                                fontSize: 15
+                            )
+                        ),
+                        Text("• Information about the vehicles involved in the accident" ,
+                            style: TextStyle(color: Colors.white,
+                                decoration: TextDecoration.none,
+                                fontSize: 15
+                            )
+                        ),
+                        Text("• Description of the road and any other environmental issues" ,
+                            style: TextStyle(color: Colors.white,
+                                decoration: TextDecoration.none,
+                                fontSize: 15
+                            )
+                        ),
+                        Text("• Any video or surveillance equipment that may have documented the accident" ,
+                            style: TextStyle(color: Colors.white,
+                                decoration: TextDecoration.none,
+                                fontSize: 15
+                            )
+                        ),
+                        Text("• Injuries" ,
+                            style: TextStyle(color: Colors.white,
+                                decoration: TextDecoration.none,
+                                fontSize: 15
+                            )
+                        ),
+                      ],
+                    ),
                   ),
                 ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(height: 30),
+                    SizedBox(
+                      width: 275,
+                      height: 50,
+                      child: CupertinoButton(
+                          color: Theme.of(context).colorScheme.primary,
+                          child: Text('Call Police', style: TextStyle(color: Theme.of(context).colorScheme.onBackground)),
+                          onPressed: () {}
+                      ),
+                    ),
+                    Container(height: 15),
+                    SizedBox(
+                      width: 275,
+                      height: 50,
+                      child: CupertinoButton(
+                          color: Theme.of(context).colorScheme.primary,
+                          child: Text('Next', style: TextStyle(color: Theme.of(context).colorScheme.onBackground)),
+                          onPressed: () {
+                            Navigator.of(context).pushReplacementNamed('/Tips');
+                          }
+                      ),
+                    ),
+                  ],
+                )
               ],
-            )
-          ],
-        ),
+            ),
+          ),
 
 
-      ],
+        ],
+      ),
     );
   }
 }
-
