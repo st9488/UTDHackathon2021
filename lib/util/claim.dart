745f64ebbb8c30e0fc096f1f384ce2f82bbcc8e1
Shamitha Thumma <66390052+st9488@users.noreply.github.com>
2021-11-13 18:52:47 -0600

    very simple claim class

---
+++ lib/util/claim.dart
@@ -0,0 +1,10 @@
+import 'package:utd_hackathon2021/util/person.dart';
+import 'package:utd_hackathon2021/util/vehicle.dart';
+
+class Claim {
+  List<List<Person>> everyone;
+  List<List<Vehicle>> vehicles;
+
+  Claim({required this.everyone, required this.vehicles});
+
+}
