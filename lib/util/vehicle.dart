89462b72860995af4e42e0e692fffdc4bffc1f5b
Shamitha Thumma <66390052+st9488@users.noreply.github.com>
2021-11-13 18:55:31 -0600

    vehicle

---
+++ lib/util/vehicle.dart
@@ -0,0 +1,16 @@
+class Vehicle
+{
+  String make;
+  String model;
+  String licenseNum;
+  int year;
+
+  Vehicle(String make, String model, String license, int year)
+      : this.make = make,
+        this.model = model,
+        licenseNum = license,
+        this.year = year;
+
+  @override
+  String toString() => 'Car: $make ' ' $model' ' $year' 'License Plate: $licenseNum';
+}
