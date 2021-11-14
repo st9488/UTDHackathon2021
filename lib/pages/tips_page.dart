552179ab2370d1700146b7edc9f784c1297bc3a8
mkm-programmer <53355393+mkm-programmer@users.noreply.github.com>
2021-11-13 21:26:11 -0600

    Vehicle Object

--- lib/pages/tips_page.dart
+++ lib/pages/tips_page.dart
@@ -21,8 +21,34 @@ class _TipsPageState extends State<TipsPage>{
     return Stack(
       children:[
         const Background(),
-        Text("data")
-      ]
+        Column(
+        children:[
+          Padding(padding: EdgeInsets.symmetric(vertical: 30)),
+          Text("IMPORTANT TIPS", style: TextStyle(decoration: TextDecoration.none, fontFamily: 'BabasNeue', fontSize: 40, color: Colors.red)),
+          Padding(padding: EdgeInsets.symmetric(vertical: 20)),
+          Expanded(
+            flex: 1,
+            child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Text(
+                  "1. Take photos of all vehicles involved and the accident scene, if it is safe to do so.\n" +
+                   "\n2. Make sure to call the police. Tell them the following:\n" +
+                    "  a. Date and time of the incident\n" +
+                    "  b. Personal information of parties\n\t\tinvolved\n" +
+                    "  c. Statements from the drivers\n" +
+                    "  d. Statements from any witnesses\n" +
+                    "  e. Information about the vehicles involved \t\tin the accident\n" +
+                    "  f. Description of the road and any other \n\t\tenvironmental issues\n" +
+                    "  g. Any video or surveillance evidence \n\t\tabout the accident" +
+                    "  h. Any injuries\n" +
+                  "\n3. Do not sign any documents unless it is for the police or your insurance agent.\n" +
+                  "\n4. Be polite, but don't tell anyone the accident was your fault, even if you think it was", style: TextStyle(decoration: TextDecoration.none, fontFamily: 'BabasNeue', fontSize: 20, color: Colors.red)
+                  ,)
+            )
+        )
+        ],
+        )
+        ]
     );
   }
 }
