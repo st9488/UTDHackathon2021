9eae7cb3295997b4fb2c4f3a2d81e95fc6b7c57c
Matthew Chimitt <mmchimitt@gmail.com>
2021-11-14 02:32:03 -0600

    Added the accident_locator.dart and update the pubspec.yaml and AndroidManifest.xml to include the Geocoding and the Geolocator flutter plugins

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:utd_hackathon2021/pages/driver_information.dart';
 import 'package:utd_hackathon2021/pages/eye_witness_page.dart';
 import 'package:utd_hackathon2021/pages/insurance_page.dart';
+//import 'package:utd_hackathon2021/pages/driver_information.dart';
 import 'package:utd_hackathon2021/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'package:utd_hackathon2021/pages/main_page.dart';
 import 'package:utd_hackathon2021/pages/panic_page.dart';
 import 'package:utd_hackathon2021/pages/tips_page.dart';
 import 'package:utd_hackathon2021/pages/vehicle_info_page.dart';
+import 'util/user.dart';
+import 'util/services/auth.dart';
 
-void main() {
+
+
+Future<void> main () async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -17,49 +26,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'HackUTD 2021',
-      theme: ThemeData(
-        colorScheme: const ColorScheme(
-            // Lighter Pink Color
-            primary: Color.fromRGBO(240, 23, 22, 1),
+    return StreamProvider<Users?>.value(
+        value: AuthService().user,
+    initialData: null,
+      child: MaterialApp(
+        title: 'HackUTD 2021',
+        theme: ThemeData(
+          colorScheme: const ColorScheme(
+              // Lighter Pink Color
+              primary: Color.fromRGBO(240, 123, 255, 1),
 
-            // Darker Variant Of Primary Pink
-            primaryVariant: Color.fromRGBO(186, 5, 211, 1),
+              // Darker Variant Of Primary Pink
+              primaryVariant: Color.fromRGBO(186, 5, 211, 1),
 
-            // Light Blue Color
-            secondary: Color.fromRGBO(27, 221, 247, 1),
+              // Light Blue Color
+              secondary: Color.fromRGBO(27, 221, 247, 1),
 
-            // Darker Variant of Primary Blue
-            secondaryVariant: Color.fromRGBO(25, 85, 189, 1),
+              // Darker Variant of Primary Blue
+              secondaryVariant: Color.fromRGBO(25, 85, 189, 1),
 
-            // White Surface Color
-            surface: Color.fromRGBO(255, 255, 255, 1),
+              // White Surface Color
+              surface: Color.fromRGBO(255, 255, 255, 1),
 
-            // Black Background Color
-            background: Color.fromRGBO(240, 23, 22, 1),
+              // Black Background Color
+              background: Color.fromRGBO(240, 23, 22, 1),
 
-            // Red Error Color
-            error: Color.fromRGBO(176, 0, 32, 1),
+              // Red Error Color
+              error: Color.fromRGBO(176, 0, 32, 1),
 
-            // Black Color On Primary/Secondary/Surface
-            onPrimary: Color.fromRGBO(0, 0, 0, 1),
-            onSecondary: Color.fromRGBO(0, 0, 0, 1),
-            onSurface: Color.fromRGBO(0, 0, 0, 1),
+              // Black Color On Primary/Secondary/Surface
+              onPrimary: Color.fromRGBO(0, 0, 0, 1),
+              onSecondary: Color.fromRGBO(90, 90, 90, 0.5),
+              onSurface: Color.fromRGBO(0, 0, 0, 1),
 
-            // White On Background/Error
-            onBackground: Color.fromRGBO(255, 255, 255, 1),
-            onError: Color.fromRGBO(255, 255, 255, 1),
+              // White On Background/Error
+              onBackground: Color.fromRGBO(255, 255, 255, 1),
+              onError: Color.fromRGBO(255, 255, 255, 1),
 
-            // Overall Brightness Level
-            brightness: Brightness.dark
-        ),
-        fontFamily: 'BabasNeue',
-        textTheme: const TextTheme (
-          headline1: TextStyle(fontSize: 36, color: Color.fromRGBO(0, 0, 0, 1)),
-          headline2: TextStyle(fontSize: 26, color: Color.fromRGBO(0, 0, 0, 1)),
-          headline3: TextStyle(fontSize: 20, color: Color.fromRGBO(0, 0, 0, 1)),
-          bodyText1: TextStyle(fontSize: 12, color: Color.fromRGBO(0, 0, 0, 1)),
+              // Overall Brightness Level
+              brightness: Brightness.dark
+          ),
+          fontFamily: 'BabasNeue',
+          textTheme: const TextTheme (
+            headline1: TextStyle(fontSize: 36, color: Color.fromRGBO(0, 0, 0, 1)),
+            headline2: TextStyle(fontSize: 26, color: Color.fromRGBO(0, 0, 0, 1)),
+            headline3: TextStyle(fontSize: 20, color: Color.fromRGBO(0, 0, 0, 1)),
+            bodyText1: TextStyle(fontSize: 12, color: Color.fromRGBO(0, 0, 0, 1)),
 
         )
       ),
@@ -85,6 +97,6 @@ class MyApp extends StatelessWidget {
         "/Eyewitness": (context) => const Eyewitness(list: []),
 
       },
-    );
+    ));
   }
 }
