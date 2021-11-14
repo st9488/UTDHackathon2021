3fc5a48915928b183f9fbb0f6dbefacd01b05191
mkm-programmer <53355393+mkm-programmer@users.noreply.github.com>
2021-11-13 18:37:19 -0600

    Vehicle Object

--- lib/pages/insurance_page.dart
+++ lib/pages/insurance_page.dart
@@ -10,14 +10,27 @@ class InsurancePage extends StatefulWidget{
 }
 
 class _InsurancePageState extends State<InsurancePage>{
+<<<<<<< HEAD
 
   final TextEditingController _textController1 = TextEditingController(text: 'company');
   final TextEditingController _textController2 = TextEditingController(text: 'numbers');
 
+=======
+  @override
+  late TextEditingController _textController1;
+  late TextEditingController _textController2;
+  void initState()
+  {
+    super.initState();
+    _textController1 = TextEditingController(text: 'company');
+    _textController2 = TextEditingController(text: 'numbers');
+  }
+>>>>>>> 8d8494c (Vehicle Object)
   @override
   Widget build(BuildContext context)
   {
     return Stack(
+<<<<<<< HEAD
         alignment: Alignment.center,
         children: [
           const Background(),
@@ -80,6 +93,52 @@ class _InsurancePageState extends State<InsurancePage>{
               ]
           )
         ]
+=======
+      alignment: Alignment.center,
+      children: [
+        const Background(),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(padding: EdgeInsets.symmetric(vertical: 60)),
+            Text('Insurance Information',style: TextStyle(decoration: TextDecoration.none, fontFamily: 'BabasNeue', fontSize: 30, color: Colors.red)
+            ),
+            Padding(padding: EdgeInsets.symmetric(vertical: 20)),
+            Text('Insurance Company',
+              style: TextStyle(decoration: TextDecoration.none, fontFamily: 'BabasNeue', fontSize: 40, color: Colors.red)
+            ),
+            CupertinoTextField(
+              placeholder: 'Company Name',
+              style: TextStyle(color: Colors.white),
+              onChanged: (String value)
+              {
+                _textController1.value;
+              },
+            ),
+            Padding(padding: EdgeInsets.symmetric(vertical: 20)),
+            Text('Insurance number',
+              style: TextStyle(decoration: TextDecoration.none, fontFamily: 'BabasNeue', fontSize: 40, color: Colors.red),
+            ),
+            CupertinoTextField(
+              placeholder: 'Insurance Number',
+              style: TextStyle(color: Colors.white),
+              onChanged: (String value)
+              {
+                _textController1.value;
+              },
+            ),
+            Padding(padding: EdgeInsets.symmetric(vertical: 20)),
+            CupertinoButton(
+                child: Text("Continue"),
+                onPressed: (){
+
+                },
+                color: Colors.red
+            )
+          ]
+        )
+      ]
+>>>>>>> 8d8494c (Vehicle Object)
     );
   }
 }
